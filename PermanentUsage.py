7cbd945b4c6148a39fe8bd5163de4fcd2bd02626
Matheus Vieira <matheusvieira0101@gmail.com>
2024-01-28 17:47:06 -0300

    chore: create functions

---
+++ PermanentUsage.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+class PermanentUsageReport():
+    def __init__(self) -> None:
+        load_dotenv()
+        pass
+
+    def retrieveDataFromDB(self):
+        pass
+
+    def groupByConsecutiveDates(self):
+        pass
+
+    def groupResults(self):
+        pass
+
+    def exportResults(self):
+        pass
